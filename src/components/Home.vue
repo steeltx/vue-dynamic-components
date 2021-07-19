ea69be32b9464c99f5f92cfb936af466258af0cf
steeltx <realsteel.gnu@gmail.com>
2021-07-18 19:27:14 -0500

    Mostrando componentes dinamicamente

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("Home component unmounted");
+  },
+  activated() {
+    console.log("Home Component Activated");
+  },
+  deactivated() {
+    console.log("Home Component Deactivated");
+  },
+};
+</script>
